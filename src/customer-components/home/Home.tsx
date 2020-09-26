c186f525326d217cfb9b509d129dfc400d396c4d
janaldoustorres <jat.torres@gmail.com>
2020-09-26 20:39:27 +0800

    added navbar to homepage

--- src/customer-components/home/Home.tsx
+++ src/customer-components/home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import logo from "../../minanamanila_thin.png";
+import logo from "../minanamanila_thin-removebg-preview.png";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import { Link } from "react-router-dom";
 import "./Home.scss";
 import { useFeatures } from "@paralleldrive/react-feature-toggles";
 import Button from "react-bootstrap/Button";
 import { Routes } from "Routes";
+import ArrowBackIosIcon from "@material-ui/icons/Menu";
+import Navbar from "react-bootstrap/Navbar";
 
 const defaultMessage = "Name:\nOrder:\nPayment:\nDelivery:";
 const uriDefaultMessage = encodeURIComponent(defaultMessage);
@@ -16,9 +18,17 @@ function Home() {
 
   return (
     <div className="home-container">
+      <Navbar className="custom-navbar">
+        <div className="flex-1-only">
+          <ArrowBackIosIcon />
+        </div>
+        <Navbar.Brand className="nav-brand" href="/">
+          <img src={logo} alt="Logo" className="logo" />
+        </Navbar.Brand>
+        <div className="flex-1-only"></div>
+      </Navbar>
       <Jumbotron>
         <div className="company-name">
-          <img src={logo} alt="Logo" className="logo" />
           <a
             className="btn btn-primary btn-lg"
             href="#how-to-order"
