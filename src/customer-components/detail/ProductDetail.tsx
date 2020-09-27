22239b9d175802a9d72ca57cddf8ce6c11420d67
janaldoustorres <jat.torres@gmail.com>
2020-09-27 16:29:39 +0800

    improved detail page

--- src/customer-components/detail/ProductDetail.tsx
+++ src/customer-components/detail/ProductDetail.tsx
@@ -3,6 +3,7 @@ import { MyNavbar } from "customer-components/navbar/Navbar";
 import "./ProductDetail.scss";
 import Button from "react-bootstrap/Button";
 import { formatCurrency } from "utils/CurrencyFormatterUtil";
+import ReactMarkdown from "react-markdown";
 
 export interface ProductDetailProps {
   username: string;
@@ -11,6 +12,9 @@ export interface ProductDetailProps {
   price: number;
   size: string;
   title: string;
+  condition: string;
+  itemsSold: number;
+  dateListed: string;
 }
 
 export const ProductDetailWithApi: React.FC<{}> = () => {
@@ -19,14 +23,17 @@ export const ProductDetailWithApi: React.FC<{}> = () => {
       username="@janaldous"
       location="Manila, Philippines"
       title="Vintage Ed Hardy camo cargo shorts"
-      productDescription="Size 30” waist. Inside leg 15”.
-      Full length 28”. Rise 14”.
-      
-      Free standard UK shipping.
-      
+      productDescription="Size 30” waist. Inside leg 15”.<br>
+      Full length 28”. Rise 14”.<br>
+      <br>
+      Free standard UK shipping.<br>
+      <br>
       90s 00s Y2K Vintage Ed Hardy Vintage Camo Cargo Shorts Ed Hardy Shorts Ed Hardy Camouflage"
       price={200}
       size={"30in"}
+      condition={"Used - Like New"}
+      itemsSold={807}
+      dateListed={"8h ago"}
     />
   );
 };
@@ -45,21 +52,41 @@ export const ProductDetail: React.FC<ProductDetailProps> = (props) => {
         </div>
         <div className="col product-info">
           <div className="seller-container">
-            <div className="profile-pic"></div>
-            <div className="seller-info">
-              <div className="username">{props.username}</div>
-              <div className="location">{props.location}</div>
+            <div className="seller-profile">
+              <div className="profile-pic"></div>
+              <div className="seller-info">
+                <div className="username">{props.username}</div>
+                <div className="location">{props.location}</div>
+              </div>
+            </div>
+            <div className="seller-actions mt-1 d-flex">
+              <div className="items-sold">{props.itemsSold} Sold</div>
+              <div className="items-sold">{props.dateListed}</div>
+              <Button variant="secondary" onClick={handleContactSeller}>
+                Contact Seller
+              </Button>
             </div>
           </div>
           <div className="product-description-container mt-3">
-            <div className="font-weight-bold description-title">{props.title}</div>
-            <div className="description-price">{formatCurrency(props.price)}</div>
-            <div className="mt-3">{props.productDescription}</div>
+            <div className="font-weight-bold description-title">
+              {props.title}
+            </div>
+            <div className="description-price">
+              {formatCurrency(props.price)}
+            </div>
+            <div className="mt-3">
+              <ReactMarkdown source={props.productDescription} escapeHtml={false} />
+            </div>
             <div className="mt-3">{props.size}</div>
+            <div className="mt-3">{props.condition}</div>
           </div>
-          <div className="contact-seller-container">
-            <Button variant="primary" onClick={handleContactSeller}>
-              Contact Seller
+          <div className="options-container mt-3 d-flex">
+            <Button
+              variant="primary"
+              onClick={handleContactSeller}
+              className="contact-seller-btn"
+            >
+              Buy Now
             </Button>
           </div>
         </div>
