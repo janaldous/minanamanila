9de1812a5aafa7e69337bf980c141bf666e503f2
janaldoustorres <jat.torres@gmail.com>
2020-09-30 00:45:30 +0800

    updated text on top of featured pic in home page

--- src/customer-components/home/Home.tsx
+++ src/customer-components/home/Home.tsx
@@ -10,8 +10,11 @@ function Home() {
   return (
     <div className="home-container">
       <div className="featured-picture position-relative">
-        <img src={featuredPicture} alt="Featured" className="w-100 picture"/>
-        <div className="words h1">Minana Manila</div>
+        <img src={featuredPicture} alt="Featured" className="w-100 picture" />
+        <div className="words">
+          <span className="first-line">This is Minana Manila.</span>
+          <br />Your new favorite go-to thrift store.
+        </div>
       </div>
       <section id="items">
         <div className="categories d-flex">
@@ -22,32 +25,6 @@ function Home() {
           ))}
         </div>
         <ProductGrid maxRows={5} />
-      </section>
-      <section id="the-bread">
-        <div className="row">
-          <div className="section-title center">Your Online Ukay-Ukay</div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="description">
-            <p> 
-              Freshly baked and healthy banana bread with almonds, walnuts,
-              cinnamon, lemon zest and muscovado sugar.
-            </p>
-            <p>
-              Crunchy and nutty on the top and light and moist in the inside.
-            </p>
-            <p>
-              Only <b>₱165</b> per loaf (delivery included)
-            </p>
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <Link to={Routes.Products}>
-            <Button variant="primary" size="lg">
-              Order
-            </Button>
-          </Link>
-        </div>
       </section>
     </div>
   );
