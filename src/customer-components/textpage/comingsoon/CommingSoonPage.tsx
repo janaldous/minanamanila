56111c5211963514880271c6567a691f5d67038f
Jat Torres <jat.torres@gmail.com>
2020-12-22 00:22:23 +0800

    added api call

--- src/customer-components/textpage/comingsoon/CommingSoonPage.tsx
+++ src/customer-components/textpage/comingsoon/CommingSoonPage.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 import { TextPage, TextPageProps } from "../TextPage";
+import {
+  Product,
+  PublicControllerApiFactory,
+} from "../../../api/minanamanila-api-client/api";
 
 export const CommingSoonPage: React.FC<TextPageProps> = (props) => {
+  const [products, setProducts] = React.useState<Array<Product>>();
+
+  React.useEffect(() => {
+    const getProducts = async () => {
+      const config = {
+        basePath: "http://localhost:8080",
+      };
+
+      const publicApi = PublicControllerApiFactory(config);
+      const result = await publicApi.getProductsUsingGET(0, 10);
+      console.log(result.data);
+      setProducts(result.data);
+    };
+
+    getProducts();
+  }, []);
+
   return (
-    <TextPage
-      {...props}
-    >
-      <div className="d-flex justify-content-center align-items-center coming-soon">Coming Soon</div>
+    <TextPage {...props}>
+      <div className="d-flex justify-content-center align-items-center coming-soon">
+        Coming Soon
+      </div>
+      <div>
+        {products?.map((product) => (
+          <div key={product.id}>{product.name}</div>
+        ))}
+      </div>
     </TextPage>
   );
 };
